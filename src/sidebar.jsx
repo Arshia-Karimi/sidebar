b68fbec03781df23cec5f1fc705793dcd82b6989
arshia <arshia.karimi@gmail.com>
2023-12-27 16:27:16 +0330

    end

--- src/sidebar.jsx
+++ src/sidebar.jsx
@@ -1,13 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./sidebar.css";
 
 const App = () => {
   const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const ser = localStorage.getItem("item");
+    if (ser) {
+      const localButton = JSON.parse(ser);
+      setOpen(localButton);
+    }
+  }, []);
+
+  const openButton = () => {
+    const a = !open;
+    setOpen(a);
+    localStorage.setItem("item", JSON.stringify(a));
+  };
+
   return (
     <div>
       <button
         className={`claw ${open ? "close" : "open"}`}
-        onClick={() => setOpen(!open)}
+        onClick={openButton}
       ></button>
       <div className={`sidebar ${open ? "show" : undefined}`}>
         <h2>sidebar</h2>
